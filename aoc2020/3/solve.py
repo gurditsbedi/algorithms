920d435d035a07b2fd565f8ffd7c2a901552c01d
Gurdit <gurditsbedi@gmail.com>
2020-12-04 17:01:26 +0530

    aoc2020 Q3.2

--- aoc2020/3/solve.py
+++ aoc2020/3/solve.py
@@ -1,6 +1,23 @@
 
 
 
+def calc(tree, r, c, step_right, step_down):
+    count = 0
+    i = 0
+    j = 0
+    while i < r and j < c:
+
+        if trees[i][j] == '#':
+            count += 1
+
+        if i + step_down >= r:
+            return count
+
+        i = (i+step_down) % r
+        j = (j+step_right) % c
+
+    return count
+
 
 with open("./input", "r") as f:
     trees = [x.strip() for x in f.readlines()]
@@ -9,14 +26,16 @@ with open("./input", "r") as f:
     c = len(trees[0])
     answer_one = 0
 
-    i = 0
-    j = 0
-    run = True
 
-    while i < r and j < c:
-        if trees[i][j] == '#':
-            answer_one += 1
-        i += 1
-        j = (j+3) % c
+    answer_one = calc(trees, r, c, 3, 1)
 
     print(answer_one)
+
+    answer_two = 1
+    answer_two *= calc(trees, r, c, 1, 1)
+    answer_two *= calc(trees, r, c, 3, 1)
+    answer_two *= calc(trees, r, c, 5, 1)
+    answer_two *= calc(trees, r, c, 7, 1)
+    answer_two *= calc(trees, r, c, 1, 2)
+
+    print(answer_two)
