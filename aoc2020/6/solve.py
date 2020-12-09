33fd91b2a8a89e7bee384c85dbe224e0225e66aa
Gurdit <gurditsbedi@gmail.com>
2020-12-09 14:43:55 +0530

    aoc2020 Q6.2

--- aoc2020/6/solve.py
+++ aoc2020/6/solve.py
@@ -8,15 +8,24 @@ with open("./input", "r") as f:
     curr_count = 0
     exists = defaultdict(int)
 
+    count_two = 0
+    group_size = 0
+
     for l in f.readlines():
         line = l.strip()
 
-        if len(line) == 0:
+        if not line:
             count_one += len(exists.values())
+            count_two += sum(1 for x in exists.values() if x == group_size)
+            group_size = 0
             exists.clear()
+            continue
 
         for c in line:
-            exists[c] = 1
+            exists[c] += 1
+
+        group_size += 1
 
 
     print(count_one)
+    print(count_two)
