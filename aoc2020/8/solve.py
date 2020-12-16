4af34c9686d0246bf1d8a771261fa1baf76c34e1
Gurdit <gurditsbedi@gmail.com>
2020-12-16 18:09:54 +0530

    aoc2020 Q8.2

--- aoc2020/8/solve.py
+++ aoc2020/8/solve.py
@@ -1,28 +1,42 @@
 
 
-accumulator_one = 0
-with open("./input", "r") as f:
-    intructions = []
-    for l in f.readlines():
-        line = l.strip().split(" ")
-        intructions.append((line[0], int(line[1])))
-
-    # print(intructions)
-
+def run(intructions):
+    acc = 0
     visited = [False for _ in intructions]
     i = 0
-    while True:
-        # print(intructions[i])
+    while i < len(intructions):
         if visited[i] == True:
-            print(accumulator_one)
-            break
+            return (True, acc)
 
         visited[i] = True
         if intructions[i][0] == "acc":
-            accumulator_one += intructions[i][1]
+            acc += intructions[i][1]
             i += 1
         elif intructions[i][0] == "jmp":
             i += intructions[i][1]
         elif intructions[i][0] == "nop":
             i += 1
 
+    return (False, acc)
+
+
+with open("./input", "r") as f:
+    intructions = []
+    for l in f.readlines():
+        line = l.strip().split(" ")
+        intructions.append((line[0], int(line[1])))
+
+    print(run(intructions)[1])
+
+    for i in range(len(intructions)):
+        new_instructions = intructions[:]
+
+        if intructions[i][0] == "nop":
+            new_instructions[i] = ("jmp", new_instructions[i][1])
+        elif intructions[i][0] == "jmp":
+            new_instructions[i] = ("nop", new_instructions[i][1])
+
+        res = run(new_instructions)
+        if res[0] == False:
+            print(res[1])
+            break
