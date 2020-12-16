51d5d1ca1252c409bc8d1d65178b8b51878a821b
Gurdit <gurditsbedi@gmail.com>
2020-12-16 17:41:23 +0530

    aoc2020 Q7.2

--- aoc2020/7/solve.py
+++ aoc2020/7/solve.py
@@ -25,6 +25,34 @@ with open("./input", "r") as f:
         for t in ins:
             mapper[t].append(" ".join(frm.split(" ")[:-2]))
 
-    count_one = 0
     count_one = recursor(word)
     print(len(everyone))
+
+
+mappertwo = defaultdict(list)
+
+
+def recursor_two(bag):
+    if not mappertwo[bag]:
+        return 0
+    count = 0
+    for subbag, subbag_count in mappertwo[bag]:
+        count += subbag_count * (1 + recursor_two(subbag))
+    return count
+
+
+with open("./input", "r") as f:
+    for l in f.readlines():
+        line = l.strip()
+
+        frm, to = line.split("contain")
+        frm = " ".join(frm.split()[:-1])
+        for x in to[:-1].split(","):
+            a = x.split()
+            if a[0] == "no":
+                mappertwo[frm] = []
+            else:
+                t = (" ".join(a[1:-1]), int(a[0]))
+                mappertwo[frm].append(t)
+
+    print(recursor_two(word))
